c70d5929720cf7daa7f11c834060de94c9f7a00d
TeaForT4 <anton.lindahl2@elev.ga.ntig.se>
2022-10-06 15:32:23 +0200

    Login + Add Items

--- authentication.php
+++ authentication.php
@@ -15,9 +15,19 @@
         $count = mysqli_num_rows($result);  
           
         if($count == 1){  
-            echo "<h1><center> Login successful </center></h1>";  
+            echo "<h1><center> Login successful (Administratör) </center></h1>";  
+            header("Location: adminPage.html");
+            exit();
         }  
         else{  
-            echo "<h1> Login failed. Invalid username or password.</h1>";  
+            $sql = "select *from låntagare where Namn = '$username' and Lösenord = '$password'";  
+            $result = mysqli_query($con, $sql);  
+            $row = mysqli_fetch_array($result, MYSQLI_ASSOC);  
+            $count = mysqli_num_rows($result);  
+            if ($count == 1){
+                echo "<h1><center> Login successful (Låntagare) </center></h1>";  
+            } else{
+                echo "<h1> Login failed. Invalid username or password.</h1>";  
+            }
         }     
 ?>  
