fcd1c69bf5601ab6c153a898b18adcf0d0eabff3
TeaForT4 <anton.lindahl2@elev.ga.ntig.se>
2022-10-11 13:49:15 +0200

    Fixes + Add Creator

--- addEBookToSystem.php
+++ addEBookToSystem.php
@@ -5,6 +5,9 @@
     $playtime = $_POST['playtime'];  
     $utgivning = $_POST['utgivning'];  
     $isbn = $_POST['isbn'];  
+    $författare = $_POST['författare'];
+    $författare2 = $_POST['författare2'];
+    $författare3 = $_POST['författare3']; 
       
         //to prevent from mysqli injection  
         $titel = stripcslashes($titel);  
@@ -13,22 +16,35 @@
         $utgivning = stripcslashes($utgivning);  
         $isbn = stripcslashes($isbn);  
         $författare = stripcslashes($författare);  
+        $författare2 = stripcslashes($författare2);  
+        $författare3 = stripcslashes($författare3);  
         $titel = mysqli_real_escape_string($con, $titel);  
         $genre = mysqli_real_escape_string($con, $genre);  
         $playtime = mysqli_real_escape_string($con, $playtime);  
         $utgivning = mysqli_real_escape_string($con, $utgivning);  
         $isbn = mysqli_real_escape_string($con, $isbn); 
         $författare = mysqli_real_escape_string($con, $författare);
+        $författare2 = mysqli_real_escape_string($con, $författare2);
+        $författare3 = mysqli_real_escape_string($con, $författare3);
 
 
         $sql = "INSERT INTO ebok (Titel, Genre, Media, Speltid, Utgivningsar, ISBN) values ('$titel','$genre','E-bok','$playtime','$utgivning','$isbn')"; 
         $result = mysqli_query($con, $sql);
         
-        if ($con->query($sql) === TRUE){
-            $last_id = $con->insert_id;
-        }
+        $last_id = $con->insert_id;
+        
         $sql = "INSERT INTO connect (Personid, Ebokid) values ('$författare','$last_id')"; 
         $result = mysqli_query($con, $sql);
+
+        if (!empty($författare2)){
+            $sql = "INSERT INTO connect (Personid, Bokid) values ('$författare2','$last_id')"; 
+            $result = mysqli_query($con, $sql);
+        }
+
+        if (!empty($författare3)){
+            $sql = "INSERT INTO connect (Personid, Bokid) values ('$författare3','$last_id')"; 
+            $result = mysqli_query($con, $sql);
+        }
 
         header("Location: eBookAdded.html");
         exit();
--- addRefBookToSystem.php
+++ addRefBookToSystem.php
@@ -1,10 +1,13 @@
 <?php      
-    include('connection.php');  
-    $titel = $_POST['titel'];  
-    $genre = $_POST['genre'];  
-    $sidor = $_POST['sidor'];  
-    $utgivning = $_POST['utgivning'];  
-    $isbn = $_POST['isbn'];  
+    include('connection.php');
+    $titel = $_POST['titel'];
+    $genre = $_POST['genre'];
+    $sidor = $_POST['sidor'];
+    $utgivning = $_POST['utgivning'];
+    $isbn = $_POST['isbn'];
+    $författare = $_POST['författare'];
+    $författare2 = $_POST['författare2'];
+    $författare3 = $_POST['författare3'];  
       
         //to prevent from mysqli injection  
         $titel = stripcslashes($titel);  
@@ -12,15 +15,36 @@
         $sidor = stripcslashes($sidor);  
         $utgivning = stripcslashes($utgivning);  
         $isbn = stripcslashes($isbn);  
+        $författare = stripcslashes($författare);  
+        $författare2 = stripcslashes($författare2);  
+        $författare3 = stripcslashes($författare3); 
         $titel = mysqli_real_escape_string($con, $titel);  
         $genre = mysqli_real_escape_string($con, $genre);  
         $sidor = mysqli_real_escape_string($con, $sidor);  
         $utgivning = mysqli_real_escape_string($con, $utgivning);  
         $isbn = mysqli_real_escape_string($con, $isbn);  
+        $författare = mysqli_real_escape_string($con, $författare);
+        $författare2 = mysqli_real_escape_string($con, $författare2);
+        $författare3 = mysqli_real_escape_string($con, $författare3);
 
         $sql = "INSERT INTO refBok (Titel, Genre, Antal_sidor, Utgivningsar, ISBN) values ('$titel','$genre','$sidor','$utgivning','$isbn')"; 
         $result = mysqli_query($con, $sql);
         
+        $last_id = $con->insert_id;
+        
+        $sql = "INSERT INTO connect (Personid, Bokid) values ('$författare','$last_id')"; 
+        $result = mysqli_query($con, $sql);
+        
+        if (!empty($författare2)){
+            $sql = "INSERT INTO connect (Personid, Bokid) values ('$författare2','$last_id')"; 
+            $result = mysqli_query($con, $sql);
+        }
+
+        if (!empty($författare3)){
+            $sql = "INSERT INTO connect (Personid, Bokid) values ('$författare3','$last_id')"; 
+            $result = mysqli_query($con, $sql);
+        }
+
         header("Location: refBookAdded.html");
         exit();
 ?>
